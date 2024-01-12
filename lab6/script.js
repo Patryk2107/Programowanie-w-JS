2c4a2ca6ba1ff1a91545caf9ec70beda24b8c78d
Patryk2107 <101318192+Patryk2107@users.noreply.github.com>
2024-01-12 14:15:22 +0100

    ball in hole end

--- lab6/script.js
+++ lab6/script.js
@@ -1,90 +1,63 @@
 const ballEl = document.querySelector('#ball');
 const wrapper = document.querySelector('#wrapper');
 const startButton = document.querySelector('#start');
-const holes = [];
+const restartButton = document.querySelector('#restart');
+let holeEl;
 let isGameRunning = false;
 let score = 0;
 let positionX = 20;
 let positionY = 20;
 let speedX = 0;
 let speedY = 0;
+
+const endGame = () => {
+  isGameRunning = false;
+  window.alert(Zdobyte punkty: ${score});
+  clearTimeout(endGame);
+}
+
 const getNumericValue = (element, isPositionLeft = false) => {
   return parseFloat(element.style[isPositionLeft ? 'left' : 'top'].slice(0, -2))
 }
 
-const createHoles = () => {
-  for (let i = 2; i < (window.innerWidth / 100); i++) {
-    const hole = document.createElement('div');
-    hole.classList.add('hole');
-    hole.style.left = `${100 * i + Math.random() * 75 - 95}px`;
-    hole.style.top = `${(Math.random() * window.innerHeight - 95) / 2}px`;
-    holes.push(hole);
-    wrapper.appendChild(hole);
-  }
-
-  for (let i = 2; i < (window.innerWidth / 100); i++) {
-    const hole = document.createElement('div');
-    hole.classList.add('hole');
-    hole.style.left = `${100 * i + Math.random() * 75 - 95}px`;
-    hole.style.top = `${Math.random() * (window.innerHeight / 2) + window.innerHeight / 2 - 100}px`;
-    holes.push(hole);
-    wrapper.appendChild(hole);
-  }
-  checkHoles();
-  randomizeCorrectHole(1);
-}
-
-const randomizeCorrectHole = (index) => {
-  let correctHole = Math.floor(Math.random() * holes.length);
-  if (correctHole === index && index < holes.length) {
-    index++;
-  } else {
-    index--;
-  }
-  holes[correctHole].classList.remove("hole");
-  holes[correctHole].classList.add("correctHole")
+const createNewHole = () => {
+  const hole = document.createElement('div');
+  hole.classList.add('correctHole');
+  hole.style.left = ${100 + Math.random() * 75 - 95}px;
+  hole.style.top = ${(Math.random() * window.innerHeight - 95) / 2}px;
+  wrapper.appendChild(hole);
+  holeEl = document.querySelector('.correctHole');
 }
 
 const orientationChange = (e) => {
   speedX = e.gamma / 45;
-  console.log(speedX);
   speedY = e.beta / 45;
 }
 
 const moveBall = () => {
+  if (!isGameRunning) {
+    speedX = 0;
+    speedY = 0;
+    return;
+  }
   if (positionX + speedX < window.innerWidth - 50 && positionX + speedX > 0) {
     positionX += speedX;
-    ballEl.style.left = `${positionX}px`;
+    ballEl.style.left = ${positionX}px;
   }
 
   if (positionY + speedY < window.innerHeight - 50 && positionY + speedY > 0) {
     positionY += speedY;
-    ballEl.style.top = `${positionY}px`;
+    ballEl.style.top = ${positionY}px;
   }
 
-  for (let i = 0; i < holes.length; i++) {
-    const topPosition = getNumericValue(holes[i]);
-    const leftPosition = getNumericValue(holes[i], true);
-    console.log(topPosition, leftPosition)
-    if (positionY < Math.floor(topPosition) + 50 && positionY > topPosition) {
-      if (positionX > leftPosition && positionX < Math.floor(leftPosition) + 50) {
-        if (holes[i].classList.contains('correctHole')) {
-          holes[i].classList.remove('correctHole');
-          holes.forEach(element => {
-            if (element.classList.contains("temporaryHole")) {
-              element.classList.remove("temporaryHole");
-              element.classList.add("hole");
-            }
-          })
-          holes[i].classList.add("temporaryHole");
-          score++;
-          randomizeCorrectHole(i);
-        } else if (holes[i].classList.contains('hole')) {
-          isGameRunning = false;
-          let yourScore = window.prompt(`zdobyłeś ${score} punktów`);
-          document.querySelector('#restart').hidden = false;
-        }
-      }
+
+  const topPosition = getNumericValue(holeEl);
+  const leftPosition = getNumericValue(holeEl, true);
+  if (positionY < Math.floor(topPosition) + 50 && positionY > topPosition) {
+    if (positionX > leftPosition && positionX < Math.floor(leftPosition) + 50) {
+      holeEl.remove();
+      score++;
+      createNewHole();
     }
   }
 
@@ -93,26 +66,27 @@ const moveBall = () => {
   }
 }
 
-const checkHoles = () => {
-  for (let i = 0; i < holes.length; i++) {
-    for (j = i + 1; j < holes.length; j++) {
-      if (
-        getNumericValue(holes[j], true) > getNumericValue(holes[i], true) + 75 &&
-        getNumericValue(holes[j]) > getNumericValue(holes[i]) + 75
-      ) {
-        holes[j].style.top = `${getNumericValue(holes[j]) + 50}px`;
-        holes[j].style.left = `${getNumericValue(holes[j], true) + 50}px`;
-      }
-
-    }
-  }
-}
-
 const startGame = () => {
+  setTimeout(endGame, 5_000);
   isGameRunning = true;
-  createHoles();
+  createNewHole();
   moveBall();
 }
 
-startButton.addEventListener('click', startGame);
+startButton.addEventListener('click', () => {
+  if (!isGameRunning) {
+    startGame()
+  }
+});
+
+restartButton.addEventListener('click', () => {
+  clearTimeout(endGame);
+  setTimeout(endGame, 5_000);
+  score = 0;
+  holeEl.remove();
+  createNewHole();
+  isGameRunning = true;
+  moveBall();
+})
+
 window.addEventListener('deviceorientation', orientationChange);
